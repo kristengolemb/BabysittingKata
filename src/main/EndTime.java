8aa144eab5ca7d50a8bd76385102861f3be07131
Kristen <kristengolemb@gmail.com>
2017-06-20 20:48:34 -0400

    time validation added

--- src/main/EndTime.java
+++ src/main/EndTime.java
@@ -4,6 +4,13 @@ import java.time.*;
 public class EndTime {
 	public LocalTime getEndTime() {
 		LocalTime endTime = LocalTime.of(04,00);
-		return endTime;
+		LocalTime actualEndTime = LocalTime.of(03, 30);
+		if (actualEndTime.isAfter(endTime)) {
+			System.out.println("You are only paid until 4am!");
+		}
+		else {
+			System.out.println("Have a good night!");
+		}
+		return actualEndTime;
 	}
 }
--- src/main/StartTime.java
+++ src/main/StartTime.java
@@ -3,7 +3,14 @@ import java.time.*;
 
 public class StartTime {
 	public LocalTime getStartTime() {
-	LocalTime startTime = LocalTime.of(17,00);
-	return startTime;
+		LocalTime startTime = LocalTime.of(17,00);
+		LocalTime actualArrivalTime = LocalTime.of(16, 30);
+		if (actualArrivalTime.isBefore(startTime)) {
+			System.out.println("Too early to clock in!");
+		}
+		else {
+			System.out.println("Start time accepted.");
+		}
+		return actualArrivalTime;
 	}
 }
