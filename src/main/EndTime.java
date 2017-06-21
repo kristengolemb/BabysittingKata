c8f619599ae288bd0356539df53402ad056eff79
Kristen <kristengolemb@gmail.com>
2017-06-20 20:22:56 -0400

    structure of time classes

---
+++ src/main/EndTime.java
@@ -0,0 +1,9 @@
+package main;
+import java.time.*;
+
+public class EndTime {
+	public LocalTime getEndTime() {
+		LocalTime endTime = LocalTime.of(04,00);
+		return endTime;
+	}
+}
---
+++ src/main/StartTime.java
@@ -0,0 +1,9 @@
+package main;
+import java.time.*;
+
+public class StartTime {
+	public LocalTime getStartTime() {
+	LocalTime startTime = LocalTime.of(17,00);
+	return startTime;
+	}
+}
