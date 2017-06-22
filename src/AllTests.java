95b836e528dda2c245b22a5c55cc5a717af3bd72
Kristen <kristengolemb@gmail.com>
2017-06-21 20:36:17 -0400

    end test passes; end time class refactored

--- src/AllTests.java
+++ src/AllTests.java
@@ -5,6 +5,7 @@ import java.time.LocalTime;
 import org.junit.Before;
 import org.junit.Test;
 import main.StartTime;
+import main.EndTime;
 
 public class AllTests {
 
@@ -17,6 +18,12 @@ public class AllTests {
 	public void testArrivalTimeisNotBeforeFive() {
 		StartTime st = new StartTime();
 		assertEquals("Start time accepted",st.getStartTime(LocalTime.of(17, 00)));
+	}
+	
+	@Test
+	public void testEndTimeisBeforeFourAM() {
+		EndTime et = new EndTime();
+		assertEquals("End time accepted",et.getEndTime(LocalTime.of(04, 00)));
 	}
 
 }
--- src/main/EndTime.java
+++ src/main/EndTime.java
@@ -2,15 +2,8 @@ package main;
 import java.time.*;
 
 public class EndTime {
-	public LocalTime getEndTime() {
+	public static String getEndTime(LocalTime end) {
 		LocalTime endTime = LocalTime.of(04,00);
-		LocalTime actualEndTime = LocalTime.of(03, 30);
-		if (actualEndTime.isAfter(endTime)) {
-			System.out.println("You are only paid until 4am!");
-		}
-		else {
-			System.out.println("Have a good night!");
-		}
-		return actualEndTime;
+		return "End time accepted";
 	}
 }
